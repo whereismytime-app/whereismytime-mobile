a17ad6e5a2ab213b36b6aa1e7a5f9ee0cb39c165
Fahim Ali Zain <fahimalizain@gmail.com>
2025-08-27 17:18:55 +0530

    fix: EAS Updates

---
+++ app.config.js
@@ -0,0 +1,88 @@
+export default {
+  expo: {
+    name: 'Where is My Time',
+    slug: 'whereismytime-mobile',
+    version: '1.0.0',
+    scheme: 'whereismytime-mobile',
+    web: {
+      bundler: 'metro',
+      output: 'static',
+      favicon: './assets/favicon.png',
+    },
+    plugins: [
+      'expo-router',
+      [
+        'expo-dev-launcher',
+        {
+          launchMode: 'most-recent',
+        },
+      ],
+      'expo-web-browser',
+      [
+        'expo-splash-screen',
+        {
+          backgroundColor: '#FFFFFF',
+          image: './assets/whereismytime-logo.jpg',
+          dark: {
+            image: './assets/splash.png',
+            backgroundColor: '#000000',
+          },
+          imageWidth: 200,
+        },
+      ],
+      '@react-native-firebase/app',
+      '@react-native-firebase/auth',
+      '@react-native-firebase/crashlytics',
+      [
+        'expo-build-properties',
+        {
+          ios: {
+            useFrameworks: 'static',
+          },
+        },
+      ],
+      [
+        '@react-native-google-signin/google-signin',
+        {
+          iosUrlScheme: 'com.googleusercontent.apps._some_id_to_be_replaced_',
+        },
+      ],
+      'expo-sqlite',
+    ],
+    experiments: {
+      typedRoutes: true,
+      tsconfigPaths: true,
+    },
+    orientation: 'portrait',
+    icon: './assets/icon.png',
+    userInterfaceStyle: 'light',
+    splash: {
+      image: './assets/splash.png',
+      resizeMode: 'contain',
+      backgroundColor: '#ffffff',
+    },
+    assetBundlePatterns: ['**/*'],
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: 'com.fahimalizain.whereismytime',
+      googleServicesFile: process.env.GOOGLE_SERVICES_INFO_PLIST ?? './GoogleService-Info.plist',
+      infoPlist: {
+        ITSAppUsesNonExemptEncryption: false,
+      },
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: './assets/adaptive-icon.png',
+        backgroundColor: '#ffffff',
+      },
+      package: 'com.fahimalizain.whereismytime',
+      googleServicesFile: process.env.GOOGLE_SERVICES_JSON ?? './google-services.json',
+    },
+    extra: {
+      router: {},
+      eas: {
+        projectId: 'cc13d83a-3f22-4419-bb3b-ecfdcb8585bd',
+      },
+    },
+  },
+};
